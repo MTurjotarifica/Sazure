719f9a4aae1a362f88ac6fee92844691cfe131ee
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-01-28 00:31:04 +0800

    Update handler.py

--- handler.py
+++ handler.py
@@ -14,28 +14,28 @@ from flask import Flask, request, Response
 
 
 
-# def lambda_handler(event, context):
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(
-os.environ['SIGNING_SECRET'], '/slack/events', app)
+def lambda_handler(event, context):
+    app = Flask(__name__)
+    slack_event_adapter = SlackEventAdapter(
+    os.environ['SIGNING_SECRET'], '/slack/events', app)
 
-#getting slack_bot_token from our stored environment variable
-client = WebClient(token=os.environ['SLACK_TOKEN'])
-#obtains bot id
-BOT_ID = client.api_call("auth.test")['user_id'] #gives us the bot id
-signature_verifier = SignatureVerifier(os.environ["SIGNING_SECRET"])
-@app.route('/slack/interactive-endpoint', methods=['GET','POST'])
-def interactive_trigger():
-    print("trigger works")
+    #getting slack_bot_token from our stored environment variable
+    client = WebClient(token=os.environ['SLACK_TOKEN'])
+    #obtains bot id
+    BOT_ID = client.api_call("auth.test")['user_id'] #gives us the bot id
+    signature_verifier = SignatureVerifier(os.environ["SIGNING_SECRET"])
+    @app.route('/slack/interactive-endpoint', methods=['GET','POST'])
+    def interactive_trigger():
+        print("trigger works")
      
-@app.route('/hello', methods=['POST'])
-def hello():
+    @app.route('/hello', methods=['POST'])
+    def hello():
         
-    print("hello works")
-    client.chat_postMessage(channel='#random', 
-                                        text="hello world  ",
-                                        )
-    #returning empty string with 200 response
-    return '', 200    
+        print("hello works")
+        client.chat_postMessage(channel='#random', 
+                                            text="hello world  ",
+                                            )
+        #returning empty string with 200 response
+        return '', 200
 
 
