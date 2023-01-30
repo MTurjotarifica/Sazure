cb975adab85512d8b1e8526eff22733fb30e8582
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-01-30 19:21:41 +0800

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,37 +1,26 @@
-import slack
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
-from slackeventsapi import SlackEventAdapter
-from slack_sdk.signature import SignatureVerifier
+import logging
 import os
-import pandas as pd
-import numpy as np
 import json
 import urllib
+
+import azure.functions as func
 from flask import Flask, request, Response
-
-from pathlib import Path              #to load environment variable
-# from dotenv import load_dotenv  
-# env_path = Path('.') / '.env'
-# load_dotenv(dotenv_path = env_path)
-
+from slack_sdk import WebClient
+from slackeventsapi import SlackEventAdapter
+from slack_sdk.signature import SignatureVerifier
 
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(
     os.environ['SIGNING_SECRET'], '/slack/events', app)
 
-#getting slack_bot_token from our stored environment variable
 client = WebClient(token=os.environ['SLACK_TOKEN'])
-#obtains bot id
-BOT_ID = client.api_call("auth.test")['user_id'] #gives us the bot id
+BOT_ID = client.api_call("auth.test")['user_id']
 signature_verifier = SignatureVerifier(os.environ["SIGNING_SECRET"])
 
 @app.route('/slack/interactive-endpoint', methods=['GET','POST'])
 def interactive_trigger():
     print("trigger works")
     payload = json.loads(request.form["payload"])
-    # do something with the payload
-    # ...
     if payload["type"] == "interactive_message":
         # code to handle interactive messages
         # ...
@@ -42,10 +31,65 @@ def interactive_trigger():
         # code to handle dialog submissions
         # ...
     return Response(status=200)
-     
+
 @app.route('/hello', methods=['POST'])
 def hello():   
     print("hello works")
     client.chat_postMessage(channel='#random', 
                             text="hello world  ",
                         )
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    return app
+
+# import slack
+# from slack_sdk import WebClient
+# from slack_sdk.errors import SlackApiError
+# from slackeventsapi import SlackEventAdapter
+# from slack_sdk.signature import SignatureVerifier
+# import os
+# import pandas as pd
+# import numpy as np
+# import json
+# import urllib
+# from flask import Flask, request, Response
+
+# from pathlib import Path              #to load environment variable
+# # from dotenv import load_dotenv  
+# # env_path = Path('.') / '.env'
+# # load_dotenv(dotenv_path = env_path)
+
+
+# app = Flask(__name__)
+# slack_event_adapter = SlackEventAdapter(
+#     os.environ['SIGNING_SECRET'], '/slack/events', app)
+
+# #getting slack_bot_token from our stored environment variable
+# client = WebClient(token=os.environ['SLACK_TOKEN'])
+# #obtains bot id
+# BOT_ID = client.api_call("auth.test")['user_id'] #gives us the bot id
+# signature_verifier = SignatureVerifier(os.environ["SIGNING_SECRET"])
+
+# @app.route('/slack/interactive-endpoint', methods=['GET','POST'])
+# def interactive_trigger():
+#     print("trigger works")
+#     payload = json.loads(request.form["payload"])
+#     # do something with the payload
+#     # ...
+#     if payload["type"] == "interactive_message":
+#         # code to handle interactive messages
+#         # ...
+#     elif payload["type"] == "block_actions":
+#         # code to handle block actions
+#         # ...
+#     elif payload["type"] == "dialog_submission":
+#         # code to handle dialog submissions
+#         # ...
+#     return Response(status=200)
+     
+# @app.route('/hello', methods=['POST'])
+# def hello():   
+#     print("hello works")
+#     client.chat_postMessage(channel='#random', 
+#                             text="hello world  ",
+#                         )
