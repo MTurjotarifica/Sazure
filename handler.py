ac9a3e380cc7b16bca9245694bafc24280c1d154
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-02-02 05:51:05 +0800

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,30 +1,73 @@
 import logging
+import os
 import json
-import azure.functions as func
-from flask import Flask, request
-
 import requests
 
-response = requests.get("https://sazurebotapp.azurewebsites.net/hello")
-if response.status_code == 200:
-    print("The endpoint is accessible")
-else:
-    print("The endpoint is not accessible")
+import azure.functions as func
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_sdk.signature import SignatureVerifier
 
-app = Flask(__name__)
-
-@app.route('/hello', methods=['POST'])
-def hello():
-    # Your code to handle the request
-    # ...
-    return "Hello World", 200
-
-@func.HttpTrigger
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    # Verify the Slack request signature
+    signature_verifier = SignatureVerifier(os.environ["SLACK_SIGNING_SECRET"])
+    try:
+        signature_verifier.validate(
+            req.headers.get("X-Slack-Request-Timestamp"),
+            req.headers.get("X-Slack-Signature"),
+            req.get_body()
+        )
+    except Exception as e:
+        return func.HttpResponse("Signature verification failed", status_code=400)
+
+    # Parse the request body
     request_body = req.get_json()
-    # Your code to handle the request
-    # ...
-    return func.HttpResponse(json.dumps({"message": "Hello World"}), 200)
+    command = request_body["command"]
+    text = request_body["text"]
+
+    # Handle the command
+    if command == "/hello":
+        # Your code to handle the command goes here
+        # ...
+        response = {
+            "response_type": "in_channel",
+            "text": "Your command was processed successfully!"
+        }
+        return func.HttpResponse(json.dumps(response), mimetype="application/json")
+
+    return func.HttpResponse("Unknown command", status_code=400)
+
+
+
+
+
+# import logging
+# import json
+# import azure.functions as func
+# from flask import Flask, request
+
+# import requests
+
+# response = requests.get("https://sazurebotapp.azurewebsites.net/hello")
+# if response.status_code == 200:
+#     print("The endpoint is accessible")
+# else:
+#     print("The endpoint is not accessible")
+
+# app = Flask(__name__)
+
+# @app.route('/hello', methods=['POST'])
+# def hello():
+#     # Your code to handle the request
+#     # ...
+#     return "Hello World", 200
+
+# @func.HttpTrigger
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+#     request_body = req.get_json()
+#     # Your code to handle the request
+#     # ...
+#     return func.HttpResponse(json.dumps({"message": "Hello World"}), 200)
 
 
 # import logging
